5acc5b20d3622287eb29a0af075584cf29114d0e
ChristophP <deedop@hotmail.de>
2017-05-14 16:23:37 +0200

    refactor

--- src/I18Next.elm
+++ src/I18Next.elm
@@ -55,36 +55,37 @@ decodeTree =
         ]
 
 
+foldTree : Data.Translations -> Dict String Tree -> String -> Data.Translations
+foldTree initialValue dict namespace =
+    Dict.foldl
+        (\key val acc ->
+            let
+                newNamespace key =
+                    if String.isEmpty namespace then
+                        key
+                    else
+                        namespace ++ "." ++ key
+            in
+                case val of
+                    Leaf str ->
+                        Dict.insert (newNamespace key) str acc
+
+                    Branch dict ->
+                        foldTree acc dict (newNamespace key)
+        )
+        initialValue
+        dict
+
+
 mapTreeToDict : Tree -> Translations
 mapTreeToDict tree =
-    let
-        foldTree initialValue dict namespace =
-            Dict.foldl
-                (\key val acc ->
-                    let
-                        newNamespace key =
-                            if String.isEmpty namespace then
-                                key
-                            else
-                                namespace ++ "." ++ key
-                    in
-                        case val of
-                            Leaf str ->
-                                Dict.insert (newNamespace key) str acc
+    case tree of
+        Branch dict ->
+            foldTree Dict.empty dict ""
+                |> Translations
 
-                            Branch dict ->
-                                foldTree acc dict (newNamespace key)
-                )
-                initialValue
-                dict
-    in
-        case tree of
-            Branch dict ->
-                foldTree Dict.empty dict ""
-                    |> Translations
-
-            _ ->
-                initialTranslations
+        _ ->
+            initialTranslations
 
 
 {-| Translate a value at a given string.
